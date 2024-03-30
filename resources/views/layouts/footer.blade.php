0d410c34927ab63bf7a41cce2acf0f1d7328b586
safa <ahmedsafa0759@gmail.com>
2024-03-30 16:54:34 +0600

    footer final

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,35 @@
+<footer>
+    <div class="container-fluid footer_div col-md-12 align-items-center">
+        <div class="container-fluid logos mx-auto col-md-6 d-flex justify-content-center">
+            <a class="d-flex justify-content-center col-4 col-md-2" target="_blank" href="https://www.facebook.com/profile.php?id=100079941048298"><i class="d-flex text-white justify-content-center mt-5 col-md-2 fs-1 fa-brands fa-facebook"></i></a>
+            <a class="d-flex justify-content-center col-4 col-md-2" target="_blank" href="https://www.facebook.com/profile.php?id=100079941048298"><i class="d-flex text-white justify-content-center mt-5 col-md-2 fs-1 fa-brands fa-twitter"></i></a>
+            <a class="d-flex justify-content-center col-4 col-md-2" target="_blank" href="https://www.facebook.com/profile.php?id=100079941048298"> <i class="d-flex text-white justify-content-center mt-5 col-md-2 fs-1 fa-brands fa-instagram"></i></a>
+        </div>
+        <h1 class="col-12 text-center text-dark mt-4 fs-3 align-center">Plot 21, Road 28, Block K, Banani, Dhaka-1213</h1>
+        <div class="container-fluid mt-5 after_h1">
+            <div class="row justify-content-center">
+                <div class="col-sm-6 col-md-3 text-center">
+                    <i class="text-light fs-2 fa-solid fa-phone"></i>
+                    <span class="text-decoration-underline text-dark d-block mt-2">01786061922</span>
+                </div>
+                <div class="col-sm-6 col-md-3 text-center">
+                    <i class="text-light fs-2 fa-regular fa-envelope"></i>
+                    <span class="text-decoration-underline text-dark d-block mt-2">ahmedsafa0759@gmail.com</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex container-fluid col-md-12 d-flex justify-content-around footer_footer_div">
+        <div class="mt-3 for_small col-md-6">
+            <p class="d-flex justify-content-center text-light">Copyright <i class="d-flex mt-1 justify-content-center fa fa-copyright"></i> Ahmed Nur E Safa</p>
+        </div>
+        <div class="mt-3 for_small col-md-6">
+            <p class="d-flex justify-content-center text-light">Powered By - Topkapi Builders</p>
+        </div>
+    </div>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
